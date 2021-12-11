94970f1d714b3ee3c7eec22cb8cefc715cc35f6a
Matt Spraggs <matthew.spraggs@gmail.com>
2021-12-11 17:37:14 +0000

    Modify day 7 to use golden section search

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -2,6 +2,8 @@ use std::collections::HashMap;
 use std::env;
 use std::fs;
 
+const GOLDEN_RATIO: f64 = 1.618033988749895;
+
 fn parse_data(data: &str) -> Result<HashMap<i32, i32>, String> {
     let positions = data
         .split(',')
@@ -20,27 +22,27 @@ fn parse_data(data: &str) -> Result<HashMap<i32, i32>, String> {
     Ok(bucketed_positions)
 }
 
-fn compute_fuel_const(positions: &HashMap<i32, i32>, position: i32) -> i32 {
+fn compute_fuel_const(positions: &HashMap<i32, i32>, position: f64) -> f64 {
     positions
         .iter()
-        .map(|(&k, &v)| (k - position).abs() * v)
+        .map(|(&k, &v)| (k as f64 - position).abs() * v as f64)
         .sum()
 }
 
-fn compute_fuel_monotonic(positions: &HashMap<i32, i32>, position: i32) -> i32 {
+fn compute_fuel_monotonic(positions: &HashMap<i32, i32>, position: f64) -> f64 {
     positions
         .iter()
         .map(|(&k, &v)| {
-            let num_steps = (k - position).abs();
-            let usage = num_steps * (num_steps + 1) / 2;
-            usage * v
+            let num_steps = (k as f64 - position).abs();
+            let usage = num_steps * (num_steps + 1.0) / 2.0;
+            usage * v as f64
         })
         .sum()
 }
 
 fn compute_optimal_fuel_usage(
     positions: &HashMap<i32, i32>,
-    compute_fuel: fn(&HashMap<i32, i32>, i32) -> i32,
+    compute_fuel: fn(&HashMap<i32, i32>, f64) -> f64,
 ) -> Result<(i32, i32), String> {
     let min = positions
         .keys()
@@ -49,18 +51,24 @@ fn compute_optimal_fuel_usage(
         .ok_or("Input data empty.".to_string())?;
     let max = positions.keys().copied().max().unwrap();
 
-    let mut min_pos = min;
-    let mut min_fuel = compute_fuel(&positions, min_pos);
+    let mut lower = min as f64;
+    let mut upper = max as f64;
 
-    for i in (min + 1)..(max + 1) {
-        let fuel = compute_fuel(&positions, i);
-        if fuel < min_fuel {
-            min_pos = i;
-            min_fuel = fuel;
+    while (upper - lower).abs() > f32::EPSILON as f64 {
+        let offset = (upper - lower) / GOLDEN_RATIO;
+        let new_lower = upper - offset;
+        let new_upper = lower + offset;
+        let new_fuel_lower = compute_fuel(&positions, new_lower);
+        let new_fuel_upper = compute_fuel(&positions, new_upper);
+
+        if new_fuel_lower < new_fuel_upper {
+            upper = new_upper;
+        } else {
+            lower = new_lower;
         }
     }
 
-    Ok((min_pos, min_fuel))
+    Ok((lower.round() as i32, compute_fuel(&positions, lower.round()) as i32))
 }
 
 fn main() -> Result<(), String> {
